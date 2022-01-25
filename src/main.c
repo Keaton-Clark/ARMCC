3e972db42db5f2619cc006fad64277021da0ca82
Clark <keatonclark2@gmail.com>
2022-01-24 22:42:45 -0800

    updated readFile function to be dynamically allocated. Reduced overall memory usage by a couple hundred bytes

--- src/main.c
+++ src/main.c
@@ -8,9 +8,7 @@ bool readFile(char* file, char** lines);
 uint32_t evalAsmLn(char* asmLn);
 
 int main (int argc, char* argv[]) {
-	if (argc != 2) {
-	       printf("Incorrect number of arguments given.\n%d: Given\n1: Required\n", argc - 1);
-	} else {
+	if (argc == 2) {
 		if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
 				printf("\nUsage:\n%s [file or flag]\n\nFlags:\n-h or --help\tShow this message\n", argv[0]);
 				return 0;
@@ -25,21 +23,27 @@ int main (int argc, char* argv[]) {
 				printf("%x\n---------------------------\n", evalAsmLn(asmLines[i]));
 			}
 		}
+	} else {
+		printf("Incorrect number of arguments given.\n%d: Given\n1: Required\n", argc - 1);
 	}
 	return 0;
 }
 
+ 
+
+
 bool readFile(char* file, char** lines) {
-	FILE* fptr;
-	fptr = fopen(file, "r");
+	FILE* fptr = fopen(file, "r");
 	if (fptr == NULL) {
 		return false;
 	}
-	size_t i = 0;
-	do { //I think I need to do better memory allocation but for now I will just allocate in excess
-		lines[i] = (char*)malloc(100); 
-		i++;
-	} while (fgets(lines[i-1], 100, fptr) != NULL);
+	size_t bufsize = 32;
+	char* buffer = (char*)malloc(bufsize);
+	for (size_t i = 0; fgets(buffer, bufsize, fptr) != NULL; i++) {
+		lines[i] = (char*)malloc(strlen(buffer));
+		strcpy(lines[i], buffer);
+	}
+	free(buffer);
 	fclose(fptr);
 	return true;
 }
@@ -48,8 +52,8 @@ uint32_t evalAsmLn(char* asmLn) {
 	//using strtok_r for safety
 	char* tmp = asmLn;
 	char* op = strtok_r(asmLn, " ", &tmp);
-	if (strcmp(op, "ADD") == 0) {	//cool little "one-liner" that gets each of the two substrings with strtok, converts them to longs with strtol, and then masks them as uint32_t and finally adds them
-		return (uint32_t)strtol(strtok_r(NULL, " ", &tmp), NULL, 0)
+	if (strcmp(op, "ADD") == 0) { //Instead of the inevitable "if... else if" tree that will result with more command, next project I will update this to be a hash function inside a switch case 
+		return (uint32_t)strtol(strtok_r(NULL, " ", &tmp), NULL, 0) //cool little "one-liner" that gets each of the two substrings with strtok, converts them to longs with strtol, and then masks them as uint32_t and finally adds them
 			+
 			(uint32_t)strtol(strtok_r(NULL, " ", &tmp), NULL, 0);
 	}	
