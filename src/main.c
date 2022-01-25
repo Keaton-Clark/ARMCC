43b4d4d5ea2be53ed198c1e7badf93f4152f4649
Clark <keatonclark2@gmail.com>
2022-01-25 15:11:41 -0800

    reformatting

--- src/main.c
+++ src/main.c
@@ -1,10 +1,15 @@
+//Name: Keaton Clark
+//Class: CS 219.1001
+//Title: Programming Project 1
+//Date: 1/25/22
+//Purpose: ASM interpreter written in C
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
 
-bool readFile(char* file, char** lines);
+bool readFile(const char* file, char** lines);
 uint32_t evalAsmLn(char* asmLn);
 
 int main (int argc, char* argv[]) {
@@ -25,6 +30,7 @@ int main (int argc, char* argv[]) {
 		}
 	} else {
 		printf("Incorrect number of arguments given.\n%d: Given\n1: Required\n", argc - 1);
+		return -1;
 	}
 	return 0;
 }
@@ -32,7 +38,7 @@ int main (int argc, char* argv[]) {
  
 
 
-bool readFile(char* file, char** lines) {
+bool readFile(const char* file, char** lines) {
 	FILE* fptr = fopen(file, "r");
 	if (fptr == NULL) {
 		return false;
@@ -52,9 +58,13 @@ uint32_t evalAsmLn(char* asmLn) {
 	//using strtok_r for safety
 	char* tmp = asmLn;
 	char* op = strtok_r(asmLn, " ", &tmp);
-	if (strcmp(op, "ADD") == 0) { //Instead of the inevitable "if... else if" tree that will result with more command, next project I will update this to be a hash function inside a switch case 
+	if (strcmp(op, "ADD") == 0) { 
 		return (uint32_t)strtol(strtok_r(NULL, " ", &tmp), NULL, 0) //cool little "one-liner" that gets each of the two substrings with strtok, converts them to longs with strtol, and then masks them as uint32_t and finally adds them
 			+
+			(uint32_t)strtol(strtok_r(NULL, " ", &tmp), NULL, 0);
+	} else if (strcmp(op, "SUB") == 0) {
+		return (uint32_t)strtol(strtok_r(NULL, " ", &tmp), NULL, 0)
+			-
 			(uint32_t)strtol(strtok_r(NULL, " ", &tmp), NULL, 0);
 	}	
 	else {
